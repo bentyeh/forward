a4b3cf92511bdedef12fc73c9265bd81cc123b35
Benjamin Yeh <bentyeh@gmail.com>
2019-07-20 06:44:05 -0700

    start.sh: SCRIPT argument correctly recognized. added support for --no-forward argument

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 #
 # Starts a remote sbatch jobs and sets up correct port forwarding.
 # 
-# Usage: bash start.sh [params_file] [SCRIPT]
+# Usage: bash start.sh [params_file] [SCRIPT] [--no-forward]
 # - params_file: path to sbatch job parameters file. default = params.sh
 # - RESOURCE: server / cluster, e.g., rice or sherlock
 # - SCRIPT: filename of sbatch script
@@ -12,11 +12,12 @@
 #     - ./sbatches/${RESOURCE}
 #     - ./${RESOURCE}
 #     See set_forward_script() in helpers.sh.
+# - --no-forward: submit job but do not setup port forwarding
 #
 # Sample usage                                  # Assumptions
 #   bash start.sh jupyter                       # params.sh exists
 #   bash start.sh sherlock/singularity-jupyter  # params.sh exists
-#   bash start.sh my_params.sh                  # NAME is set in my_params.sh
+#   bash start.sh my_params.sh                  # SCRIPT is set in my_params.sh
 #   bash start.sh my_params.sh jupyter
 # 
 # Expected variables set in params_file:
@@ -35,21 +36,39 @@
 # Unset NAME variable in case it is in use (e.g., by WSL)
 unset NAME
 
+# Check for "--no-forward" argument and shift arguments $1, $2, ... accordingly
+# - https://stackoverflow.com/questions/255898/how-to-iterate-over-arguments-in-a-bash-script
+# - https://stackoverflow.com/questions/4827690/how-to-change-a-command-line-argument-in-bash
+argc=$#
+argv=($@)
+for ((i=0; i<argc; i++)); do
+    if [[ "${argv[$i]}" = "--no-forward" ]]; then
+        no_forward="true"
+        if [[ $i -eq 0 ]]; then
+            set -- "${@:$(($i+2)):$argc}"
+        else
+            set -- "${@:1:$(($i))}" "${@:$(($i+2)):$argc}"
+        fi
+        break
+    fi
+done
+
 params_file=$1
 if [ ! -f "$params_file" ]; then
     params_file="params.sh"
-    NAME_ARG=$1
+    SCRIPT_ARG=$1
     if [ ! -f "$params_file" ]; then
         echo "Need to configure params before first run, run setup.sh!"
         exit 1
     fi
 else
-    NAME_ARG=$2
+    SCRIPT_ARG=$2
 fi
 source "$params_file"
 
-[ ! -z $NAME_ARG ] && NAME=$NAME_ARG
-[ -z $NAME ] && echo "Need to give NAME of sbatch job to run!" 1>&2 && exit 1
+[ ! -z $SCRIPT_ARG ] && SCRIPT=$SCRIPT_ARG
+[ -z $SCRIPT ] && echo "Need to give SCRIPT of sbatch job to run!" 1>&2 && exit 1
+[ -z $NAME ] && NAME=SCRIPT
 
 if [ ! -f helpers.sh ]; then
     echo "Cannot find helpers.sh script!"
@@ -99,6 +118,8 @@ command="$SLURM_COMMAND
 
 echo ${command}
 if [[ $SLURM_COMMAND = "srun" ]]; then
+    # cannot capture output of srun job submission (e.g., RESULT=`ssh ${RESOURCE} ${command} 2>&1`)
+    # and separately wait for job allocation, since srun is blocking.
     ssh ${RESOURCE} ${command} &> /dev/null &
 else
     ssh ${RESOURCE} ${command}
@@ -111,7 +132,7 @@ instruction_get_logs
 get_machine
 echo "notebook running on $MACHINE"
 
-setup_port_forwarding
+[ -z $no_forward ] && setup_port_forwarding
 
 sleep 5
 echo 
@@ -124,7 +145,8 @@ echo
 
 instruction_get_logs
 echo
+i=1
 echo "== Instructions =="
-echo "1. Password, output, and error printed to this terminal? Look at logs (see instruction above)"
-echo "2. Browser: http://$MACHINE:$PORT/ -> http://localhost:$LOCALPORT/..."
-echo "3. To end session: bash end.sh ${params_file} ${NAME}"
+echo "$i. Password, output, and error printed to this terminal? Look at logs (see instruction above)" && ((i=i+1))
+[ -z $no_forward ] && echo "2. Browser: http://$MACHINE:$PORT/ -> http://localhost:$LOCALPORT/..." && ((i=i+1))
+echo "$i. To end session: bash end.sh ${params_file} ${NAME}"
